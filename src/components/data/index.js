6e7b3bd9f5897d376827126021a6b1d3f911757a
mbarcea <mateo.brc1@gmail.com>
2026-07-21 09:59:55 +0200

    update links

--- src/components/data/index.js
+++ src/components/data/index.js
@@ -12,7 +12,7 @@ export const projects = [
         des: "Landing page for Brainwave an open AI chat app.",
         img: "covers/brainwave.png",
         iconLists: ["svg/vite.svg", "svg/react.svg", "svg/js.svg", "svg/tailwind.svg"],
-        link: "https://truhcaz.github.io/brainwave/",
+        link: "https://mbarcea.github.io/brainwave/",
     },
     {
         id: 2,
@@ -20,7 +20,7 @@ export const projects = [
         des: "Pokedex Application using the PokeAPI, design found on Dribbble",
         img: "covers/pokedex.png",
         iconLists: ["svg/react.svg", "svg/js.svg", "svg/css.svg", "svg/api.svg"],
-        link: "https://truhcaz.github.io/pokedex/",
+        link: "https://mbarcea.github.io/pokedex/",
     },
     {
         id: 3,
@@ -36,7 +36,7 @@ export const projects = [
         des: "Landing page for a travel agency in Egypt.",
         img: "covers/gotoegypt.png",
         iconLists: ["svg/react.svg", "svg/css.svg", "svg/js.svg", "svg/framer.svg"],
-        link: "https://truhcaz.github.io/gotoegypt/",
+        link: "https://mbarcea.github.io/gotoegypt/",
     },
 ];
 
@@ -44,247 +44,247 @@ export const randomProjects = [
     {
         id: 1,
         title: "Expanding Cards",
-        link: "https://truhcaz.github.io/js-50-projects/1-expanding-cards"
+        link: "https://mbarcea.github.io/js-50-projects/1-expanding-cards"
     },
     {
         id: 2,
         title: "Progress Steps",
-        link: "https://truhcaz.github.io/js-50-projects/2-progress-steps"
+        link: "https://mbarcea.github.io/js-50-projects/2-progress-steps"
     },
     {
         id: 3,
         title: "Rotating Navigation",
-        link: "https://truhcaz.github.io/js-50-projects/3-rotating-navigation"
+        link: "https://mbarcea.github.io/js-50-projects/3-rotating-navigation"
     },
     {
         id: 4,
         title: "Hidden Search",
-        link: "https://truhcaz.github.io/js-50-projects/4-hidden-search"
+        link: "https://mbarcea.github.io/js-50-projects/4-hidden-search"
     },
     {
         id: 5,
         title: "Blurry Loading",
-        link: "https://truhcaz.github.io/js-50-projects/5-blurry-loading"
+        link: "https://mbarcea.github.io/js-50-projects/5-blurry-loading"
     },
     {
         id: 6,
         title: "Scroll Animation",
-        link: "https://truhcaz.github.io/js-50-projects/6-scroll-animation"
+        link: "https://mbarcea.github.io/js-50-projects/6-scroll-animation"
     },
     {
         id: 7,
         title: "Split Landing Page",
-        link: "https://truhcaz.github.io/js-50-projects/7-split-landing-page"
+        link: "https://mbarcea.github.io/js-50-projects/7-split-landing-page"
     },
     {
         id: 8,
         title: "Form Wave",
-        link: "https://truhcaz.github.io/js-50-projects/8-form-wave"
+        link: "https://mbarcea.github.io/js-50-projects/8-form-wave"
     },
     {
         id: 9,
         title: "Soundboard",
-        link: "https://truhcaz.github.io/js-50-projects/9-soundboard"
+        link: "https://mbarcea.github.io/js-50-projects/9-soundboard"
     },
     {
         id: 10,
         title: "Dad Jokes",
-        link: "https://truhcaz.github.io/js-50-projects/10-dad-jokes"
+        link: "https://mbarcea.github.io/js-50-projects/10-dad-jokes"
     },
     {
         id: 11,
         title: "Event Keycodes",
-        link: "https://truhcaz.github.io/js-50-projects/11-event-keycodes"
+        link: "https://mbarcea.github.io/js-50-projects/11-event-keycodes"
     },
     {
         id: 12,
         title: "FAQ Collapse",
-        link: "https://truhcaz.github.io/js-50-projects/12-faq-collapse"
+        link: "https://mbarcea.github.io/js-50-projects/12-faq-collapse"
     },
     {
         id: 13,
         title: "Random Picker",
-        link: "https://truhcaz.github.io/js-50-projects/13-random-picker"
+        link: "https://mbarcea.github.io/js-50-projects/13-random-picker"
     },
     {
         id: 14,
         title: "Nav Animation",
-        link: "https://truhcaz.github.io/js-50-projects/14-nav-animation"
+        link: "https://mbarcea.github.io/js-50-projects/14-nav-animation"
     },
     {
         id: 15,
         title: "Incrementing Counter",
-        link: "https://truhcaz.github.io/js-50-projects/15-incrementing-counter"
+        link: "https://mbarcea.github.io/js-50-projects/15-incrementing-counter"
     },
     {
         id: 16,
         title: "Drink Water",
-        link: "https://truhcaz.github.io/js-50-projects/16-drink-water"
+        link: "https://mbarcea.github.io/js-50-projects/16-drink-water"
     },
     {
         id: 17,
         title: "Movie App",
-        link: "https://truhcaz.github.io/js-50-projects/17-movie-app"
+        link: "https://mbarcea.github.io/js-50-projects/17-movie-app"
     },
     {
         id: 18,
         title: "Background Slider",
-        link: "https://truhcaz.github.io/js-50-projects/18-background-slider"
+        link: "https://mbarcea.github.io/js-50-projects/18-background-slider"
     },
     {
         id: 19,
         title: "Theme Clock",
-        link: "https://truhcaz.github.io/js-50-projects/19-theme-clock"
+        link: "https://mbarcea.github.io/js-50-projects/19-theme-clock"
     },
     {
         id: 20,
         title: "Button Ripple Effect",
-        link: "https://truhcaz.github.io/js-50-projects/20-button-ripple"
+        link: "https://mbarcea.github.io/js-50-projects/20-button-ripple"
     },
     {
         id: 21,
         title: "Drag N Drop",
-        link: "https://truhcaz.github.io/js-50-projects/21-drag-n-drop"
+        link: "https://mbarcea.github.io/js-50-projects/21-drag-n-drop"
     },
     {
         id: 22,
         title: "Drawing App",
-        link: "https://truhcaz.github.io/js-50-projects/22-drawing-app"
+        link: "https://mbarcea.github.io/js-50-projects/22-drawing-app"
     },
     {
         id: 23,
         title: "Kinetic Loader",
-        link: "https://truhcaz.github.io/js-50-projects/23-kinetic-loader"
+        link: "https://mbarcea.github.io/js-50-projects/23-kinetic-loader"
     },
     {
         id: 24,
         title: "Content Placeholder",
-        link: "https://truhcaz.github.io/js-50-projects/24-content-placeholder"
+        link: "https://mbarcea.github.io/js-50-projects/24-content-placeholder"
     },
     {
         id: 25,
         title: "Sticky Navbar",
-        link: "https://truhcaz.github.io/js-50-projects/25-sticky-navbar"
+        link: "https://mbarcea.github.io/js-50-projects/25-sticky-navbar"
     },
     {
         id: 26,
         title: "Double Vertical Slider",
-        link: "https://truhcaz.github.io/js-50-projects/26-double-vertical-slider"
+        link: "https://mbarcea.github.io/js-50-projects/26-double-vertical-slider"
     },
     {
         id: 27,
         title: "Toast Notifications",
-        link: "https://truhcaz.github.io/js-50-projects/27-toast-notifications"
+        link: "https://mbarcea.github.io/js-50-projects/27-toast-notifications"
     },
     {
         id: 28,
         title: "Github Profiles",
-        link: "https://truhcaz.github.io/js-50-projects/28-github-profiles"
+        link: "https://mbarcea.github.io/js-50-projects/28-github-profiles"
     },
     {
         id: 29,
         title: "Double Click Heart",
-        link: "https://truhcaz.github.io/js-50-projects/29-double-click-heart"
+        link: "https://mbarcea.github.io/js-50-projects/29-double-click-heart"
     },
     {
         id: 30,
         title: "Auto Text Effect",
-        link: "https://truhcaz.github.io/js-50-projects/30-auto-text-effect"
+        link: "https://mbarcea.github.io/js-50-projects/30-auto-text-effect"
     },
     {
         id: 31,
         title: "Password Generator",
-        link: "https://truhcaz.github.io/js-50-projects/31-password-generator"
+        link: "https://mbarcea.github.io/js-50-projects/31-password-generator"
     },
     {
         id: 32,
         title: "Good Cheap Fast",
-        link: "https://truhcaz.github.io/js-50-projects/32-good-cheap-fast"
+        link: "https://mbarcea.github.io/js-50-projects/32-good-cheap-fast"
     },
     {
         id: 33,
         title: "Notes App",
-        link: "https://truhcaz.github.io/js-50-projects/33-notes-app"
+        link: "https://mbarcea.github.io/js-50-projects/33-notes-app"
     },
     {
         id: 34,
         title: "Animated Countdown",
-        link: "https://truhcaz.github.io/js-50-projects/34-animated-countdown"
+        link: "https://mbarcea.github.io/js-50-projects/34-animated-countdown"
     },
     {
         id: 35,
         title: "Image Carousel",
-        link: "https://truhcaz.github.io/js-50-projects/35-image-carousel"
+        link: "https://mbarcea.github.io/js-50-projects/35-image-carousel"
     },
     {
         id: 36,
         title: "Hoverboard",
-        link: "https://truhcaz.github.io/js-50-projects/36-hoverboard"
+        link: "https://mbarcea.github.io/js-50-projects/36-hoverboard"
     },
     {
         id: 38,
         title: "Mobile Tab Navigation",
-        link: "https://truhcaz.github.io/js-50-projects/38-mobile-tab-navigation"
+        link: "https://mbarcea.github.io/js-50-projects/38-mobile-tab-navigation"
     },
     {
         id: 39,
         title: "Password Strength",
-        link: "https://truhcaz.github.io/js-50-projects/39-password-strength"
+        link: "https://mbarcea.github.io/js-50-projects/39-password-strength"
     },
     {
         id: 40,
         title: "Magic Boxes",
-        link: "https://truhcaz.github.io/js-50-projects/40-magic-boxes"
+        link: "https://mbarcea.github.io/js-50-projects/40-magic-boxes"
     },
     {
         id: 41,
         title: "Verify Account",
-        link: "https://truhcaz.github.io/js-50-projects/41-verify-account"
+        link: "https://mbarcea.github.io/js-50-projects/41-verify-account"
     },
     {
         id: 42,
         title: "Live User Filter",
-        link: "https://truhcaz.github.io/js-50-projects/42-live-user-filter"
+        link: "https://mbarcea.github.io/js-50-projects/42-live-user-filter"
     },
     {
         id: 43,
         title: "Feedback UI",
-        link: "https://truhcaz.github.io/js-50-projects/43-feedback-ui"
+        link: "https://mbarcea.github.io/js-50-projects/43-feedback-ui"
     },
     {
         id: 44,
         title: "Custom Range Slider",
-        link: "https://truhcaz.github.io/js-50-projects/44-custom-range"
+        link: "https://mbarcea.github.io/js-50-projects/44-custom-range"
     },
     {
         id: 45,
         title: "Netflix Mobile Nav",
-        link: "https://truhcaz.github.io/js-50-projects/45-netflix-nav"
+        link: "https://mbarcea.github.io/js-50-projects/45-netflix-nav"
     },
     {
         id: 46,
         title: "Quizz App",
-        link: "https://truhcaz.github.io/js-50-projects/46-quizz-app"
+        link: "https://mbarcea.github.io/js-50-projects/46-quizz-app"
     },
     {
         id: 47,
         title: "Testimonial Box",
-        link: "https://truhcaz.github.io/js-50-projects/47-testimonial-box"
+        link: "https://mbarcea.github.io/js-50-projects/47-testimonial-box"
     },
     {
         id: 48,
         title: "Random Image Feed",
-        link: "https://truhcaz.github.io/js-50-projects/48-random-image-feed"
+        link: "https://mbarcea.github.io/js-50-projects/48-random-image-feed"
     },
     {
         id: 49,
         title: "Todo List",
-        link: "https://truhcaz.github.io/js-50-projects/49-todo-list"
+        link: "https://mbarcea.github.io/js-50-projects/49-todo-list"
     },
     {
         id: 50,
         title: "Halloween Catcher",
-        link: "https://truhcaz.github.io/js-50-projects/50-halloween-catcher"
+        link: "https://mbarcea.github.io/js-50-projects/50-halloween-catcher"
     }
 ];
 
@@ -298,7 +298,7 @@ export const socialMedia = [
     {
         id: 1,
         img: "svg/git.svg",
-        link: "https://github.com/Truhcaz"
+        link: "https://github.com/mbarcea"
     },
     {
         id: 2,
